e54a41d842f6b2a95d07a26d52cb9e5deb34fd0c
jainesh patel <pateljainesh1708@gmail.com>
2022-07-26 17:33:08 +0530

    competitive programming

---
+++ src/sda_quetion.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class sda_quetion {
+    public static void main(String[] args) {
+
+
+//***********************************************************************************************************************************************
+        // find out  min max number in array
+
+   /*  Scanner sc = new Scanner(System.in);
+        System.out.print("enter the element : ");
+        int element = sc.nextInt();
+
+        int [] array = new int[element];
+        int max = Integer.MIN_VALUE;
+        int min =Integer.MAX_VALUE;
+
+
+
+        for (int i=0; i<element; i++){
+            System.out.print("enter the number ");
+            array[i] = sc.nextInt();
+
+        }
+        for (int i=0; i<element; i++){
+            if (array[i]>=max){
+                max = array[i];
+
+            }
+            if (array[i]<=min){
+                min = array[i];
+            }
+        }
+
+
+        System.out.println("the max element is : " + max);
+        System.out.println("the min element is : " + min);*/
+//***********************************************************************************************************************************************
+        // reverse the array
+    /*    Scanner sc = new Scanner(System.in);
+        System.out.print("enter the element :");
+        int element = sc.nextInt();
+        int [] arr = new  int[element];
+
+        for (int j=0; j<element; j++) {
+            System.out.print("enter the number : ");
+            arr[j] = sc.nextInt();
+
+        }
+            int temp;
+            int l = arr.length;
+            int n = Math.floorDiv(l,2);
+
+            for (int i=0; i<n; i++){
+                temp = arr[i];
+                arr[i] = arr[l-1-i];
+                arr[l-1-i] = temp;
+            }
+
+            for (int e:arr) {
+                System.out.print(e + " ");
+
+            }
+*/
+//***********************************************************************************************************************************************
+
+      }
+    }
+
