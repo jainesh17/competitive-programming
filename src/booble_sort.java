c05f5b1dd39a103b8291457ccb27cba69f200311
jainesh patel <pateljainesh1708@gmail.com>
2022-07-30 00:08:23 +0530

    competitive programming

---
+++ src/booble_sort.java
@@ -0,0 +1,24 @@
+
+
+public class booble_sort {
+
+
+    public static void main(String[] args) {
+
+        // bubble sort
+
+        int arr[] = {1,8,3,2,7,9,6,5};
+        for (int i=0; i<=arr.length-1; i++){
+            for (int j=0; j<arr.length-i-1; j++){
+                int temp;
+                if (arr[j]>arr[j+1]){
+                    temp = arr[j];
+                    arr[j]= arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
