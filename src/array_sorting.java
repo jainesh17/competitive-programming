893d34268e986fb8ae6d8833f83d9bb531547b55
jainesh patel <pateljainesh1708@gmail.com>
2022-08-05 14:11:46 +0530

    competitive programming

---
+++ src/array_sorting.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class array_sorting {
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 4, 3, 6};
+        Arrays.sort(arr);
+        for (int num : arr) {
+            System.out.println(num);
+        }
+
+
+    }
+}
---
+++ src/coding.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class string_char_comperision {
+    public static void str2(String str, int idx ,String inputstr){
+       char [] st1 = str.toCharArray();
+       char [] st2 = inputstr.toCharArray();
+        Arrays.sort(st1);
+        Arrays.sort(st2);
+       if ( Arrays.equals(st1,st2)){
+           System.out.println(true);
+       }
+       else {
+           System.out.println(false);
+       }
+
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String str1 = sc.nextLine();
+
+        str2(str,0,str1);
+
+    }
+}
